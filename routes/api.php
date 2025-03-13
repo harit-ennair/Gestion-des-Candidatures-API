e9ccb45180eff08f754ec9ea91cff1d7ef2b3a70
harit <harit@gmail.com>
2025-03-13 11:24:04 +0000

    Refactor API routes for improved organization and readability

--- routes/api.php
+++ routes/api.php
@@ -21,36 +21,78 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+// Route::get( '/users', [UserController::class, 'create']);
+// Route::post( '/users', [UserController::class, 'store']);
+// Route::post( '/login', [UserController::class, 'login']);
+
+// Route::middleware('auth:sanctum')->group(function () {
+    
+
+// 	Route::get( '/users/{id}', [UserController::class, 'edit']);
+// 	Route::put( '/users/{id}', [UserController::class, 'update']);
+
+	
+// 	Route::get( '/offers_users', [OffersUserController::class, 'index']);
+// 	Route::get( '/offers_users/create', [OffersUserController::class, 'create']);
+// 	Route::post( '/offers_users', [OffersUserController::class, 'store']);
+// 	Route::get( '/offers_users/{id}', [OffersUserController::class, 'show']);
+// 	Route::get( '/offers_users/{id}/edit', [OffersUserController::class, 'edit']);
+// 	Route::put( '/offers_users/{id}', [OffersUserController::class, 'update']);
+// 	Route::delete( '/offers_users/{id}', [OffersUserController::class, 'destroy']);
+
+	
+// 	Route::get( '/offers', [OffersController::class, 'index']);
+// 	Route::get( '/offers/create', [OffersController::class, 'create']);
+// 	Route::post( '/offers', [OffersController::class, 'store']);
+// 	Route::get( '/offers/{id}', [OffersController::class, 'show']);
+// 	Route::get( '/offers/{id}/edit', [OffersController::class, 'edit']);
+// 	Route::put( '/offers/{id}', [OffersController::class, 'update']);
+// 	Route::delete( '/offers/{id}', [OffersController::class, 'destroy']);
+
+// });
+
+
+
+
+
+
+
+
 Route::get( '/users', [UserController::class, 'create']);
 Route::post( '/users', [UserController::class, 'store']);
 Route::post( '/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    
-
-	Route::get( '/users/{id}', [UserController::class, 'edit']);
-	Route::put( '/users/{id}', [UserController::class, 'update']);
-
 	
-
-	Route::get( '/offers_users', [OffersUserController::class, 'index']);
-	Route::get( '/offers_users/create', [OffersUserController::class, 'create']);
-	Route::post( '/offers_users', [OffersUserController::class, 'store']);
-	Route::get( '/offers_users/{id}', [OffersUserController::class, 'show']);
-	Route::get( '/offers_users/{id}/edit', [OffersUserController::class, 'edit']);
-	Route::put( '/offers_users/{id}', [OffersUserController::class, 'update']);
-	Route::delete( '/offers_users/{id}', [OffersUserController::class, 'destroy']);
-
 	
-    
-	Route::get( '/offers', [OffersController::class, 'index']);
-	Route::get( '/offers/create', [OffersController::class, 'create']);
-	Route::post( '/offers', [OffersController::class, 'store']);
-	Route::get( '/offers/{id}', [OffersController::class, 'show']);
-	Route::get( '/offers/{id}/edit', [OffersController::class, 'edit']);
-	Route::put( '/offers/{id}', [OffersController::class, 'update']);
-	Route::delete( '/offers/{id}', [OffersController::class, 'destroy']);
+	Route::prefix('users')->group(function () {
+	
+		Route::get( '/{id}', [UserController::class, 'edit']);
+		Route::put( '/{id}', [UserController::class, 'update']);
+	});
 
+	Route::prefix('offers_users')->group(function () {
+		
+		Route::get( '/', [OffersUserController::class, 'index']);
+		Route::get( '/create', [OffersUserController::class, 'create']);
+		Route::post( '/', [OffersUserController::class, 'store']);
+		Route::get( '/{id}', [OffersUserController::class, 'show']);
+		Route::get( '/{id}/edit', [OffersUserController::class, 'edit']);
+		Route::put( '/{id}', [OffersUserController::class, 'update']);
+		Route::delete( '/{id}', [OffersUserController::class, 'destroy']);
+	});
+
+	Route::prefix('offers')->group(function () {
+
+		Route::get( '/', [OffersController::class, 'index']);
+		Route::get( '/create', [OffersController::class, 'create']);
+		Route::post( '/', [OffersController::class, 'store']);
+		Route::get( '/{id}', [OffersController::class, 'show']);
+		Route::get( '/{id}/edit', [OffersController::class, 'edit']);
+		Route::put( '/{id}', [OffersController::class, 'update']);
+		Route::delete( '/{id}', [OffersController::class, 'destroy']);
+	});
+	
 });
 
 
